4e19aa81d456fa5d62bc4875957b6470461d374c
Himanshu <himanshunavgotri@gmail.com>
2026-07-18 17:15:04 +0530

    Remove secret key.

--- src/service/weatherService.ts
+++ src/service/weatherService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_KEY = process.env.WEATHER_API_KEY;
-const BASE_URL = process.env.WEATHER_BASE_URL;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const WEATHER_BASE_URL = process.env.WEATHER_BASE_URL;
 const url = `https://api.openweathermap.org/data/2.5/weather`;
 
 export const getCurrentWeather = async (city: string) => {
@@ -9,7 +9,7 @@ export const getCurrentWeather = async (city: string) => {
   const response = await axios.get(url, {
     params: {
       q: city,
-      appid: 'cf20eeb94f4e2709b2e78e0a0ce90dba',
+      appid: WEATHER_API_KEY,
       units: "metric",
     },
   });
@@ -22,14 +22,14 @@ export const getCityWeatherAnalytics = async (city: string) => {
     axios.get(url, {
       params: {
         q: city,
-        appid: 'cf20eeb94f4e2709b2e78e0a0ce90dba',
+        appid: WEATHER_API_KEY,
         units: "metric",
       },
     }),
     axios.get(`https://api.openweathermap.org/data/2.5/forecast`, {
       params: {
         q: city,
-        appid: 'cf20eeb94f4e2709b2e78e0a0ce90dba',
+        appid: WEATHER_API_KEY,
         units: "metric",
       },
     }),
